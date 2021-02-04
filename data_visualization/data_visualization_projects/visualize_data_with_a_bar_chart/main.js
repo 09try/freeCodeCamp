5440c5303bc0ed311916333e641487055fb2ec90
09try <0994try@gmail.com>
2021-02-04 20:41:15 +0100

    progress

---
+++ data_visualization/data_visualization_projects/visualize_data_with_a_bar_chart/main.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var svg = d3.select('#main')
+        .append(svg)
+        .attr('width', 200)
+        .attr('height', 200);
+
+    console.log('done');
+});
