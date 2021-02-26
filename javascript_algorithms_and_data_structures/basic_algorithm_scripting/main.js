ff98bddad8fa2b39ddcc7f1e1ea37ea84c6f3ff2
09try <0994try@gmail.com>
2021-02-26 16:08:31 +0100

    progress

--- javascript_algorithms_and_data_structures/basic_algorithm_scripting/main.js
+++ javascript_algorithms_and_data_structures/basic_algorithm_scripting/main.js
@@ -1,12 +1,81 @@
-function frankenSplice(arr1, arr2, n) {
-    let arr3 = [];
-    for (let i = 0; i < arr2.length; i++) {
-        arr3.push(arr2[i]);
+// function frankenSplice(arr1, arr2, n) {
+//     let arr3 = [];
+//     for (let i = 0; i < arr2.length; i++) {
+//         arr3.push(arr2[i]);
+//     }
+
+//     arr3.splice(n, 0, ...arr1);
+
+//     return arr3;
+// }
+
+// console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+
+// function getIndexToIns(arr, num) {
+
+//     let myArr = arr.sort(function(a, b) {
+//         return a - b;
+//     });
+
+//     for (let i = 0; i < myArr.length; i++) {
+//         if (num <= myArr[i]) {
+//             return i;
+//         }
+//     }
+
+//     return arr.length;
+// }
+
+// console.log(getIndexToIns([5, 3, 20, 3], 5));
+
+// function mutation(arr) {
+
+//     var count = 0;
+
+//     var string0 = arr[0].toLowerCase().split("");
+//     var string1 = arr[1].toLowerCase().split("");
+
+//     for (let i = 0; i < string1.length; i++) {
+//         let ch = string1[i];
+//         if (string0.indexOf(ch) != -1) {
+//             count += 1;
+//         }
+
+//     }
+
+//     if (count >= arr[1].length) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(mutation(["Noel", "Ole"]));
+
+function chunkArrayInGroups(arr, size) {
+
+    var out = [];
+    out.push(new Array());
+
+    let column = 0;
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        out[column].push(arr[i]);
+        count += 1;
+
+        if (count == size) {
+
+            column += 1;
+            out.push(new Array());
+            count = 0;
+        }
     }
 
-    arr3.splice(n, 0, ...arr1);
+    if (out[out.length - 1].length == 0) {
+        out.pop();
+    }
 
-    return arr3;
+    return out;
 }
 
-console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
+console.log(chunkArrayInGroups([0, 1, 2, 3], 2));
